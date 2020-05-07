7d47b377f61472f32b6a829274779da072ea53ac
lamafab <42901763+lamafab@users.noreply.github.com>
2020-05-07 00:04:20 +0000

    use common function for requirements

--- src/lib.rs
+++ src/lib.rs
@@ -91,6 +91,7 @@ impl BenchmarkAnalyser {
     /// Pallet: "balances", Extrinsic: "set_balance", Lowest values: [], Highest values: [], Steps: [10], Repeat: 10
     /// u,e,extrinsic_time,storage_root_time
     /// ```
+    #[rustfmt::skip]
     pub fn parse_header(content: &ResultContent) -> Result<(), Error> {
         let mut step_entry = StepEntry::default();
 
@@ -104,48 +105,55 @@ impl BenchmarkAnalyser {
                 .split_whitespace()
                 .collect();
 
+            fn check_requirements(
+                input_key: &str,
+                input_val: &str,
+                key_name: &str,
+                len: usize,
+                val_start: &str,
+                val_end: &str,
+                to_remove: &[&str],
+            ) -> Result<String, Error> {
+                // E.g. ... == "Pallet:"
+                check(|| input_key == key_name)?;
+
+                // E.g. ... == "Extrinsic:"
+                check(|| {
+                    input_val.len() > len
+                        && input_val.starts_with(val_start)
+                        && input_val.ends_with(val_end)
+                })?;
+
+                // E.g. from `"set_balance",` -> `set_balance`
+                let mut value = String::from(input_val);
+                for r in to_remove {
+                    value = value.replace(r, "");
+                }
+
+                Ok(value)
+            }
+
             check(|| parts.len() == 13)?;
 
             // Parse pallet name
-            check(|| parts[0] == "Pallet:")?;
-            check(|| {
-                parts[1].len() > 3 && parts[1].starts_with("\"") && parts[1].ends_with("\",")
-            })?;
-
-            step_entry.pallet = String::from(parts[1].replace("\"", "").replace("\"", ""));
+            step_entry.pallet =
+                check_requirements(parts[0], parts[1], "Pallet:", 3, "\"", "\",", &["\"", ","])?;
 
             // Parse extrinsic name
-            check(|| parts[2].starts_with("Extrinsic:"))?;
-            check(|| {
-                parts[3].len() > 3 && parts[3].starts_with("\"") && parts[3].ends_with("\",")
-            })?;
-
-            step_entry.extrinsic = String::from(parts[3].replace("\"", "").replace("\"", ""));
+            step_entry.extrinsic = check_requirements(parts[2], parts[3], "Extrinsic:", 3, "\"", "\",", &["\"", ","])?;
 
             // Parse steps amount
-            let part = parts[9];
-            check(|| part == "Steps:")?;
-            check(|| part.len() > 2 && part.starts_with("[") && part.ends_with("]"))?;
+            step_entry.steps =
+                check_requirements(parts[9], parts[10], "Steps:", 2, "[", "],", &["[", ","])?
+                    .parse::<usize>()
+                    .map_err(|_| InvalidHeader)?;
 
-            step_entry.steps = part
-                .replace("[", "")
-                .replace("]", "")
-                .parse::<usize>()
-                .map_err(|_| InvalidHeader)?;
-
-            // Parse repeat amount
-            let part = parts[11];
-            check(|| part == "Steps:")?;
-            check(|| part.len() > 2 && part.starts_with("[") && part.ends_with("]"))?;
-
-            step_entry.steps = part
-                // The repeat amount does not have brackets around it,
-                // probably removed by accident. Generally not an issue,
-                // just a small inconsistency.
-                // .replace("[", "")
-                // .replace("]", "")
-                .parse::<usize>()
-                .map_err(|_| InvalidHeader)?;
+            // Parse repeat amount. The amount does not have brackets around it, probably removed by accident.
+            // Generally not an issue, just a small inconsistency.
+            step_entry.steps =
+                check_requirements(parts[11], parts[12], "Repeat:", 2, "", "", &["", ""])?
+                    .parse::<usize>()
+                    .map_err(|_| InvalidHeader)?;
         }
 
         Ok(())
