1d9e24c92748baac7ec86fe4db1a307692fc022d
lamafab <42901763+lamafab@users.noreply.github.com>
2020-05-09 21:54:42 +0200

    formatting

--- tests/overview_table.rs
+++ tests/overview_table.rs
@@ -24,9 +24,8 @@ fn test_overview_table_full_files() -> Result<(), Error> {
         ("treasury", "tip_new", 2.1406, 114.0605),
         ("balances", "transfer", 2.233, 123.3014),
         ("staking", "bond_extra", 2.2533, 125.333),
-        ("democracy", "delegate", 18.3259, 1732.5924)
+        ("democracy", "delegate", 18.3259, 1732.5924),
     ];
-
 
     let mut counter = 0;
     let list = table.list();
@@ -63,9 +62,8 @@ fn test_overview_table_shortened() -> Result<(), Error> {
         ("treasury", "tip_new", 1.8363, 83.6271),
         ("balances", "transfer", 2.4501, 145.0108),
         ("staking", "bond_extra", 2.4575, 145.7468),
-        ("democracy", "delegate", 19.6006, 1860.0573)
+        ("democracy", "delegate", 19.6006, 1860.0573),
     ];
-
 
     let mut counter = 0;
     let list = table.list();
